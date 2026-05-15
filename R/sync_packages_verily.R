6d11a48b706ceb8a339dd078e3ff5cacfd4c363b
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2026-05-15 11:27:33 -0500

    Sync packages in verily workspace

---
+++ R/sync_packages_verily.R
@@ -0,0 +1,23 @@
+#' Sync packages between Verily workspace and bucket
+#' @examples 
+#' \dontrun{
+#' # This can be placed at the top of the notebook:
+#' if (!require("aou.bucket"))
+#' {
+#'    devtools::install_github("annisjs/aou.bucket", upgrade = F)
+#'    require(aou.bucket)
+#' }
+#' sync_packages_verily()
+#' }
+#' @details Syncing is done by first syncing local packages to the bucket. Packages are then synced from the bucket to local storage.
+#' The sync_packages function can be left at the top of the script so that the workspace and bucket packages remain synchronized. 
+#' The sync_packages function is especially useful when the persistent disk has been deleted after downloading R packages. In this case,
+#' packages will remain on the bucket but not on local storage. When respawning the environment, the sync_packages function can be run and
+#' all packages from the bucket will sync to the local storage. This should save the time it takes to download and re-install R packages.
+#' @export
+sync_packages_verily <- function()
+{
+  my_bucket <- Sys.getenv("WORKSPACE_BUCKET")
+  system(stringr::str_glue("gcloud storage rsync /opt/conda/envs/jupyter/lib/R/library {my_bucket}/packages --recursive"),intern=T)
+  system(stringr::str_glue("gcloud storage rsync {my_bucket}/packages /opt/conda/envs/jupyter/lib/R/library --recursive"),intern=T)
+}
